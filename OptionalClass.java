e134a3f0265169ae00cee8a2407a964d1065ee0f
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-03-17 15:57:38 +0530

    Optional Class of() method impementation

--- OptionalClass.java
+++ OptionalClass.java
@@ -8,11 +8,13 @@ public class OptionalClass
     	 
     	 str[5] = "Go Corona";
     	 str[3] = "Corona Go";
+    	 str[7] = "COVID-19";
     	 
-    	 Optional<String> check = Optional.empty();
-    	 
+    	 Optional<String> check = Optional.empty();    	 
     	 System.out.println("Checking empty method of Optional Class : "+check);
     	 
+    	 Optional<String> checknull = Optional.of(str[3]);
+    	 System.out.println("Printing value using Optional of() method : "+checknull);
     	 
     	 
     	 
