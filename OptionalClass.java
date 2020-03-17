0927696feb736ce323345bf2746cf2931ad2d610
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-03-17 15:50:46 +0530

    Optional Class empty() method

---
+++ OptionalClass.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class OptionalClass
+{
+     public static void main(String args[])
+     {
+         String str[] = new String[10];     
+    	 
+    	 str[5] = "Go Corona";
+    	 str[3] = "Corona Go";
+    	 
+    	 Optional<String> check = Optional.empty();
+    	 
+    	 System.out.println("Checking empty method of Optional Class : "+check);
+    	 
+    	 
+    	 
+    	 
+    	 
+    	 
+     }
+
+}
