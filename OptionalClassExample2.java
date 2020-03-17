6252438e698248a752ea1258cedc3c61e4058484
Akhilesh KUmar <akhilesh.h.kumar@accenture.com>
2020-03-17 13:06:36 +0530

    testing GIT revert command

--- OptionalClassExample2.java
+++ OptionalClassExample2.java
@@ -9,12 +9,17 @@ public class OptionalClassExample2
         
         Optional<String> checknull = Optional.ofNullable(str[5]);
         
+                
         if(checknull.isPresent())
            System.out.println("Value is present");
         else
            System.out.println("There is no value in str[5]");
         
-        System.out.println("we were able to avoid abnormal termination of program");        
+        System.out.println("we were able to avoid abnormal termination of program"); 
+        
+        
+        System.out.println("Testing Revert git Command");
+                
         
     }
 	
